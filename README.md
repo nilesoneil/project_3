0deacf8f8d1ef7b9166e4ca06a28203ec7c9b8e1
Niles O'Neil <nilesoneil@gmail.com>
2022-04-22 11:48:39 -0700

    2nd test passing

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Describe: beepBoop()
 Test: "It will create an array from 0 - the user's inputted number"
 Code: beepBoop(7)
 Expected Output: [0,1,2,3,4,5,6,7]
+
+Test: "It will find all digits with 1 in them and replace it with "beep"
+Code: beepBoop(7)
+Expected Output: [0, "beep", 2, 3, 4, 5, 6, 7]
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
-$(document).ready(function() {
-  $("form#").submit(function(event) {
-    let n = 7;
-    let initialArray = [];
-    for (let i = 0; i <= n; i++) {
-      initialArray.push(i);
+function beepBoop(input) {   
+  let roboArray = [];
+  for (let index = 0; index <= input; index++) {
+      let stringIndex = index.toString();
+      let appendable = index
+    
+    if (stringIndex.includes("1")) {
+      appendable = "beep";
+    } else if (stringIndex.includes("2")) {
+      appendable = "boop";
+    } else if (stringIndex.includes("3")) {
+      appendable = "won't you be my neighbor?";
     }
+    roboArray.push(appendable);
+  } 
+  return roboArray
+}  
+  
+  
+  $(document).ready(function() {
+    $("form#").submit(function(event) {
  
-console.log(initialArray;
+
     event.preventDefault();
   });
 });
