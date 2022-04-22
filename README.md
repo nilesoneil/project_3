515c907e2124459628e880b98fc8d45f254e52cb
Niles O'Neil <nilesoneil@gmail.com>
2022-04-22 11:14:19 -0700

    first test passes

---
+++ README.md
@@ -0,0 +1,5 @@
+Describe: beepBoop()
+
+Test: "It will create an array from 0 - the user's inputted number"
+Code: beepBoop(7)
+Expected Output: [0,1,2,3,4,5,6,7]
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $("form#").submit(function(event) {
-    
+    let n = 7;
+    let initialArray = [];
+    for (let i = 0; i <= n; i++) {
+      initialArray.push(i);
+    }
+ 
+console.log(initialArray;
     event.preventDefault();
   });
 });
